d6a0c46f674629f2748ade5fea87ad894754bbb6
Aidar Valshin <Aidar_V2000@mail.ru>
2020-11-22 14:12:46 +0300

    backup 22.11.2020

--- part6/injection/TryInject.java
+++ part6/injection/TryInject.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE) // only for
+@Target(ElementType.TYPE) // only for classes
 @Retention(RetentionPolicy.RUNTIME) // use in runtime
 public @interface TryInject {
 }
